e0f20c80813625bd5721cee68af2ff060f1e542b
Mohan Dev <mohandev23091987@gmail.com>
2026-07-09 08:46:20 +0530

    pushing changes

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,14 +25,9 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
-  timeout:70000,
-  expect:{
-    timeout:3000
-  },
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    actionTimeout:6000,
-    navigationTimeout:20000,
+
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
 
--- tests/Day42_HandlingDropdowns.spec.ts
+++ tests/Day42_HandlingDropdowns.spec.ts
@@ -1,20 +1,84 @@
 import {expect,test} from '@playwright/test'
 
-test("handling elements @P1 @Login",async({page,browser,context})=>{
+test("Handing Drowns",async({page})=>{
+
+await page.goto("https://testautomationpractice.blogspot.com/");
 
 
-await page.goto("https://practice-automation.com/form-fields/");
+test.setTimeout(2*60*1000)
+// identify a dropdown 
+//select option 
 
-await page.getByLabel('Name').first().fill('Ram')
-await page.getByLabel('Name',{exact:true}).press('Tab')
 
-await page.getByLabel('Password',{exact:true}).fill('mohan')
-await page.getByLabel('Password',{exact:true}).clear()
+let countryDropDown = page.locator('#country');
 
-await page.getByLabel('Password',{exact:true}).fill('Laxman')
-console.log(await page.getByLabel('Password',{exact:true}).inputValue())
 
-await page.getByLabel('Password',{exact:true}).waitFor({
-    state:'visible',
-    timeout:10000
+
+//visible text
+await countryDropDown.selectOption('Germany')
+
+//await page.waitForTimeout(9000);
+
+// second syntax to select visible text 
+await countryDropDown.selectOption({label:'Japan'})
+//await page.waitForTimeout(9000);
+
+// by using value
+await countryDropDown.selectOption({value:'uk'})
+
+//index 
+await countryDropDown.selectOption({index:1})
+
+
+//await page.waitForTimeout(9000);
+
+
+// validations
+
+await expect(countryDropDown).toHaveValue(/Canada/i);
+//await expect(countryDropDown).toHaveCount(10);
+
+
+
+
+
+await expect(countryDropDown).toContainText('Japan')
+
+console.log(await countryDropDown.textContent()) // all options text 
+
+
+let alloptionTexts = await countryDropDown.textContent();
+await expect(alloptionTexts?.includes('Japan')).toBeTruthy()
+
+
+//get all option elements 
+////select[@id='country']/option
+
+let alloptionelements = await page.locator('#country>option').all();
+
+for(let option of alloptionelements){
+
+    let optionText = await option.textContent();
+    if(optionText === 'Japan'){
+        console.log('option is present')
+        break;
+    }
+}
+
+
+//
+
+let alloptionsText = await page.locator('#country>option').allTextContents();
+console.log(alloptionsText)
+
+
+//verify 
+
+await expect(page.locator('#country>option')).toContainText(
+
+    ['United States','Canada','United Kingdom']
+
+
+)
+
 })
