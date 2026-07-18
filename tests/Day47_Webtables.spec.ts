088d7dc3afbb18071865ed30ff9645261c8f66c3
Mohan Dev <mohandev23091987@gmail.com>
2026-07-18 10:33:49 +0530

    latest

--- tests/Day47_Webtables.spec.ts
+++ tests/Day47_Webtables.spec.ts
@@ -51,5 +51,75 @@ console.log(await rows.locator('th').allInnerTexts())
 //print amit books
 console.log(await rows.filter({hasText:'Amit'}).locator('td').nth(0).allInnerTexts())
 
+//7
+//{hasText:'Learn Java'}  => 1
+table.locator('tbody tr',{hasText:'Learn Java'}).locator('td').nth(1);
 
 })
+
+test('Deepdive into Webtables',async({page})=>{
+
+ await page.goto('https://testautomationpractice.blogspot.com/')
+
+ //identifying webtable
+ let table =  page.locator('table[name="BookTable"]');
+
+ //getting rows 
+
+ let rows = table.locator('tr');  // multiple rows 
+
+ let rowscount =await rows.count()
+console.log('number of rows',await rows.count())
+
+//get columns
+let columns = rows.locator('th')
+console.log('number of columns',await columns.count())
+
+//7
+//{hasText:'Learn Java'}  => 1
+console.log('filetering concept')
+console.log(await table.locator('tbody tr',{hasText:'Learn Java'}).locator('td').nth(1).innerText())
+
+//24
+console.log('printing the row by using all concept')
+let arryofLocators = await table.locator('tbody tr',{hasText:'Javascript'}).all()
+console.log(await arryofLocators[0].innerText())
+
+//books of any author 
+console.log('printing all author books')
+let authorRows = await table.locator('tbody tr',{hasText:'Mukesh'}).all();
+
+for(let row of authorRows){
+   let bookname = await row.locator('td').nth(0).innerText()
+    let bookprice = await row.locator('td').nth(3).innerText()
+    console.log( `${bookname} price is ${bookprice}`)
+
+}
+
+// printing all column names
+
+let allcolumns =await page.locator('#taskTable').locator('thead th').allInnerTexts()
+
+await expect(allcolumns).toContain('Name')
+await expect(allcolumns.includes('Name')).toBeTruthy()
+
+console.log('printing names under Dynamic Web Table')
+let allrowsProducttable = await page.locator('#taskTable').locator('tbody tr').all()
+for(let row of allrowsProducttable){
+
+    console.log(await row.locator('td').nth(0).innerText())
+}
+
+
+await page.locator('#productTable').locator('tbody tr',{hasText:'	Smartphone'}) .getByRole('checkbox').click()
+
+let priceofProduct = await page.locator('#productTable>tbody tr',{hasText:'Smartwatch'}).locator('td').nth(2).innerText()
+
+console.log(priceofProduct)
+
+
+
+
+})
+
+
